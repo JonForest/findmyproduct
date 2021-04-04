8dcad7bd61d615034772bd18fb0ddcbf49b7c8ff
Jonathan Hollingsworth <jonforest@gmail.com>
2021-04-05 11:45:40 +1200

    Update code and file format to include a maximum price for a book in order to filter results

--- findmyproduct.py
+++ findmyproduct.py
@@ -10,9 +10,9 @@ class Result:
         self.title = title
         self.price = price
         self.url = url
-    
 
-def get_title_details(found_title):
+
+def get_title_details(found_title, acceptable_price=50):
     """
     Receives a BeautifulSoup node and retrieves the details from it
     Returns a Result instance
@@ -22,10 +22,18 @@ def get_title_details(found_title):
         url = trademeUrl + parent['href']
         buy_now = getattr(parent.find(class_='listingBuyNowPrice'), 'string', None)
         current_bid = getattr(parent.find(class_='listingBidPrice'), 'string', None)
-        return Result(found_title.string.strip(), buy_now or current_bid, url)
     except AttributeError:
         print(f'Not able to find details for {found_title.string}')
+        return None
 
+    try:
+        price = buy_now or current_bid
+        if float(price[1:]) > acceptable_price:
+            return None
+    except ValueError:
+        print(f'Not able to cast price to float for {found_title.string}')
+
+    return Result(found_title.string.strip(), buy_now or current_bid, url)
 
 def output_details(results_list):
     for result in results_list:
@@ -35,7 +43,13 @@ def output_details(results_list):
         print('\n')
 
 
-def findbook(title):
+def findbook(title_price):
+    """
+    Accepts a tuple of title and price
+    Returns a list of Result objects
+    """
+
+    title = title_price[0]
     page = requests.get(f'{trademeUrl}/Browse/SearchResults.aspx?type=Search&searchType=193&searchString={quote_plus(title)}')
     soup = BeautifulSoup(page.content, 'html.parser')
     titles = soup(class_='title')
@@ -43,21 +57,33 @@ def findbook(title):
     results = []
     for found_title in titles:
         if title.lower() in found_title.string.strip().lower():
-            results.append(get_title_details(found_title))
+            result = get_title_details(found_title, title_price[1])
+            if result:
+                results.append(result)
 
     return results
 
 
-def get_titles():
+def get_titles_and_prices():
+    """
+    Returns a list of tuples in the format [(title,price), (title, price)...]
+    """
     # Open up the file
     with open('book_list.txt', mode='rt', encoding='utf-8') as f:
-        titles = [title.strip() for title in f.readlines()]
+        books = []
+        try:
+            books = [(book.split('-')[0].strip(), float(book.split('-')[1].strip())) for book in f.readlines()]
+        except IndexError:
+            print('Error in format of book_list.txt. All books should be <book_title(string)>-<price(number)>')
+        except ValueError:
+            print('Price should be formatted without the $, e.g. 15.00 or 15, not $15')
+        finally:
+            return books
 
-    return titles
 
 
 def main():
-    book_titles = get_titles()
+    book_titles = get_titles_and_prices()
     results = []
     for title in book_titles:
         results += findbook(title)
